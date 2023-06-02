b41ff58d067b4639e28fec1d31d7d63c6df66d5d
Mike Weaver <michael@triplewhale.com>
2023-06-02 13:26:35 -0400

    working demo!

--- src/components/StickyTable.jsx
+++ src/components/StickyTable.jsx
@@ -5,44 +5,6 @@ const StickyTable = (props) => {
   const columns = props?.columns ?? [];
   const gridSize = columns?.length ?? 0;
 
-  const tableWrapperClass = ".sticky-table-wrapper";
-  const tableClass = ".sticky-table";
-  const stickyHeaderClass = ".sticky-table-header.fixed";
-
-  const [tableScrollLeft, setTableScrollLeft] = useState(0);
-  const [showStickyHeader, setShowStickyHeader] = useState(false);
-  const [tableWrapper, setTableWrapper] = useState(false);
-  const [table, setTable] = useState(false);
-
-  function scrollListener() {
-    const stickyHeader = document.querySelector(stickyHeaderClass);
-
-    if (
-      document.querySelector("header").getBoundingClientRect().height >
-        table.getBoundingClientRect().top &&
-      !(
-        stickyHeader.getBoundingClientRect().bottom >
-        table.getBoundingClientRect().bottom - 20
-      )
-    ) {
-      toggleScrollParams(true);
-    } else {
-      toggleScrollParams();
-    }
-  }
-
-  function toggleScrollParams(add) {
-    if (add) {
-      setShowStickyHeader(true);
-    } else {
-      setShowStickyHeader(false);
-    }
-  }
-
-  function tableScrollListener(e) {
-    setTableScrollLeft(e.target.scrollLeft);
-  }
-
   function formatValue(value, type) {
     if (type === "currency") {
       return `$${value}`;
@@ -53,59 +15,23 @@ const StickyTable = (props) => {
     }
   }
 
-  useEffect(() => {
-    const stickyHeader = document.querySelector(stickyHeaderClass);
-    if (stickyHeader) stickyHeader.scrollLeft = tableScrollLeft;
-    if (tableWrapper) tableWrapper.scrollLeft = tableScrollLeft;
-  }, [tableScrollLeft]);
-
-  useEffect(() => {
-    setTable(document.querySelector(tableClass));
-    if (table)
-      document.addEventListener("scroll", scrollListener, { passive: true });
-
-    setTableWrapper(document.querySelector(tableWrapperClass));
-    if (tableWrapper)
-      tableWrapper.addEventListener("scroll", tableScrollListener, {
-        passive: true,
-      });
-
-    // cleanup/unmount
-    return () => {
-      if (table)
-        document.removeEventListener("scroll", scrollListener, {
-          passive: true,
-        });
-
-      if (tableWrapper)
-        tableWrapper.removeEventListener("scroll", tableScrollListener, {
-          passive: true,
-        });
-    };
-  }, []);
-
   return (
     <>
       {data.length > 0 && columns.length > 0 ? (
         <div className="sticky-table-wrapper">
           <div className="sticky-table">
-            {/* headers */}
-            {[...Array(2)].map((_, i) => (
-              <div
-                key={i}
-                className={`${i === 1 ? "fixed " : ""}${
-                  i === 1 && showStickyHeader ? "sticky " : ""
-                }sticky-table-header`}
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
-                }}
-              >
-                {columns.map((column, i) => (
-                  <p key={i}>{column.name}</p>
-                ))}
-              </div>
-            ))}
+            {/* header */}
+            <div
+              className="sticky-table-header"
+              style={{
+                display: "grid",
+                gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
+              }}
+            >
+              {columns.map((column, i) => (
+                <p key={i}>{column.name}</p>
+              ))}
+            </div>
             {/* data */}
             {data.map((row, i) => (
               <div
